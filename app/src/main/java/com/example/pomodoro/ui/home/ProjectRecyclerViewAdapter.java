c842b712cd9608a012922445361d331721e04fe9
Xu Zhiyong <13980668829@163.com>
2020-07-15 10:32:43 +0800

    step 2: use databinding with recyclerview.

--- app/src/main/java/com/example/pomodoro/ui/home/ProjectRecyclerViewAdapter.java
+++ app/src/main/java/com/example/pomodoro/ui/home/ProjectRecyclerViewAdapter.java
@@ -3,17 +3,17 @@ package com.example.pomodoro.ui.home;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.pomodoro.R;
 import com.example.pomodoro.database.Project;
+import com.example.pomodoro.databinding.ListItemProjectBinding;
 import com.example.pomodoro.viewModel.MainViewModel;
+
+import org.jetbrains.annotations.NotNull;
 
 import timber.log.Timber;
 
@@ -49,59 +49,56 @@ public class ProjectRecyclerViewAdapter
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item_project, parent, false);
-                //创建卡片  参数:  布局，组件组，是否是根节点
-
-        final ViewHolder holder = new ViewHolder(itemView);
-
-        //添加点击事件
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModel.setSelectedProject(holder.mItem);
-
-                //Toast.makeText(v.getContext(), "你点击了Item: "+ prj.getTitle(), Toast.LENGTH_SHORT).show();
-                Timber.d("onClick: ");
-
-                // Navigate to NotificationFragment
-                NavDirections action = HomeFragmentDirections.actionNavigationHomeToNavigationNotifications();
-                Navigation.findNavController(v).navigate(action);
-            }
-        });
-
-        return holder;
+        //创建卡片  参数:  布局，组件组，是否是根节点
+        return ViewHolder.from(parent);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         //Project project = mValues.get(position);
         Project project = getItem(position);
-        holder.bind(project);
+        holder.bind(project, this);
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public final ImageView mImageView;
-        public final TextView mTitleView;
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public ProjectRecyclerViewAdapter mAdapter;
+        public ListItemProjectBinding mBinding;
         public Project mItem;
 
-        public ViewHolder(View view) {
-            super(view);
-            mView = view;
-            mImageView = (ImageView) view.findViewById(R.id.item_image);
-            mTitleView = (TextView) view.findViewById(R.id.item_title);
+        private ViewHolder(ListItemProjectBinding binding) {
+            super(binding.getRoot());
+            this.mBinding = binding;
+            this.mBinding.getRoot().setOnClickListener(this);
         }
 
-        public void bind(Project item) {
+        @NotNull
+        public static ProjectRecyclerViewAdapter.ViewHolder from(ViewGroup parent) {
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            return new ViewHolder(ListItemProjectBinding.inflate(layoutInflater, parent, false));
+        }
+
+        public void bind(Project item, ProjectRecyclerViewAdapter adapter) {
+            mAdapter = adapter;
             mItem = item;
-            mImageView.setImageResource(item.getImageId());
-            mTitleView.setText(item.getTitle());
+            mBinding.itemImage.setImageResource(item.getImageId());
+            mBinding.itemTitle.setText(item.getTitle());
         }
 
         @Override
         public String toString() {
-            return super.toString() + " '" + mTitleView.getText() + "'";
+            return super.toString() + " '" + mBinding.itemTitle.getText() + "'";
+        }
+
+        //添加点击事件
+        public void onClick(View v) {
+            mAdapter.viewModel.setSelectedProject(mItem);
+            //Toast.makeText(v.getContext(), "你点击了Item: "+ mItem.getTitle(), Toast.LENGTH_SHORT).show();
+            Timber.d("onClick: "+mItem.getTitle());
+
+            // Navigate to NotificationFragment
+            NavDirections action = HomeFragmentDirections.actionNavigationHomeToNavigationNotifications();
+            Navigation.findNavController(v).navigate(action);
+
         }
     }
 }
